6b38b859f6136a4a5c814e8ad2b262f1de727b2d
cansijyun <42692305@qq.com>
2024-06-04 14:42:43 +0800

    fix: delete unused codes and format file

--- packages/workflows/src/original/index.ts
+++ packages/workflows/src/original/index.ts
@@ -3,11 +3,9 @@ import { writeFileSync } from 'fs';
 import { mkdirpSync as mkdirp } from 'mkdirp';
 import { basename, dirname, join } from "path";
 
-import { assetListDefaultValuesSetter, assetListOperationsOriginal, assetListPropertyRenameMap, assetListValueReplacer, chainPropertyRenameMap } from './config';
 import { publicDir, registriesDir, registry } from '../config';
 
 const registryDir = join(registriesDir, 'original')
-console.log('',registryDir);
 
 const options: RegistryBuilderOptions = {
   assetList: {
@@ -35,37 +33,37 @@ const options: RegistryBuilderOptions = {
   }
 }
 
-function run () {
+function run() {
 
 
-const builder = new RegistryBuilder(registry, options);
+  const builder = new RegistryBuilder(registry, options);
 
-builder.build(registryDir);
-builder.buildSchemas(registryDir, (str: string) => str, (_str: string) => false);
+  builder.build(registryDir);
+  builder.buildSchemas(registryDir, (str: string) => str, (_str: string) => false);
 
-// validate
+  // validate
 
-const validator = new SchemaValidator(new Registry(registryDir), {
-  allErrors: false,
-  useStrict: false
-});
-validator.validateAllData();
+  const validator = new SchemaValidator(new Registry(registryDir), {
+    allErrors: false,
+    useStrict: false
+  });
+  validator.validateAllData();
 
-const which = 'original'
-const newReg = new Registry(registryDir);
-newReg.forEachSchemas(([title, schema])=>{
-  const filename = basename(schema.path);
-  const $id = `https://chainregistry.org/schemas/${which}/${filename}`;
-  delete schema.content.$id
-  const s = {
-    $id,
-    ...schema.content
-  }
-  const folderName = join(publicDir, which);
-  const out = join(folderName, filename)
-  mkdirp(dirname(out));
-  writeFileSync(out, JSON.stringify(s, null, 2));
-});
+  const which = 'original'
+  const newReg = new Registry(registryDir);
+  newReg.forEachSchemas(([title, schema]) => {
+    const filename = basename(schema.path);
+    const $id = `https://chainregistry.org/schemas/${which}/${filename}`;
+    delete schema.content.$id
+    const s = {
+      $id,
+      ...schema.content
+    }
+    const folderName = join(publicDir, which);
+    const out = join(folderName, filename)
+    mkdirp(dirname(out));
+    writeFileSync(out, JSON.stringify(s, null, 2));
+  });
 
 }
 
