a4dc209d7256a60c1a443ce9598c2306935fb945
Dan Lynch <pyramation@gmail.com>
2024-11-13 18:40:29 -0800

    camel

---
+++ packages/workflows/src/camel.ts
@@ -0,0 +1,75 @@
+import { Registry, RegistryBuilder, RegistryBuilderOptions, SchemaValidator } from '@chain-registry/workflows';
+import { basename, dirname, join } from "path";
+
+import { assetListDefaultValuesSetter, assetListOperations, assetListPropertyRenameMap, assetListValueReplacer,chainPropertyRenameMap, publicDir, registriesDir, registry } from './config';
+import { camelCaseTransform, isValidIdentifierCamelized } from './utils';
+
+import { mkdirpSync as mkdirp } from 'mkdirp';
+import { writeFileSync } from 'fs';
+
+const registryDir = join(registriesDir, 'camel')
+
+
+const options: RegistryBuilderOptions = {
+  assetList: {
+    camelCase: true,
+    space: 2,
+    // propertyRenameMap: assetListPropertyRenameMap,
+    defaultValuesSetter: assetListDefaultValuesSetter,
+    valueReplacer: assetListValueReplacer
+  },
+  chain: {
+    camelCase: true,
+    space: 2,
+    propertyRenameMap: chainPropertyRenameMap
+  },
+  ibcData: {
+    camelCase: true,
+    space: 2
+  },
+  ops: {
+    assetList: [
+        {
+            op: 'add',
+            path: '/$defs/asset/properties/type_asset/enum/-',
+            value: 'sdk.factory'
+        },
+        {
+            op: 'add',
+            path: '/$defs/asset/properties/type_asset/enum/-',
+            value: 'bitsong'
+        }
+    ],
+    chain: [ ],
+    ibcData: []
+  }
+}
+
+const builder = new RegistryBuilder(registry, options);
+
+builder.build(registryDir);
+builder.buildSchemas(registryDir, camelCaseTransform, isValidIdentifierCamelized);
+
+// validate
+
+// const validator = new SchemaValidator(new Registry(registryDir), {
+//   allErrors: false,
+//   useStrict: false
+// });
+// validator.validateAllData();
+
+const which = 'camel'
+const newReg = new Registry(registryDir);
+newReg.forEachSchemas(([title, schema])=>{
+  const filename = basename(schema.path);
+  const $id = `https://chainregistry.org/schemas/${which}/${filename}`;
+  delete schema.content.$id
+  const s = {
+    $id,
+    ...schema.content
+  }
+  const folderName = join(publicDir, which);
+  const out = join(folderName, filename)
+  mkdirp(dirname(out));
+  writeFileSync(out, JSON.stringify(s, null, 2));
+});
